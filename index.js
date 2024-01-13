37c207cc04b43a3d44035a129d107a95f05e167b
Puru-Rana <pururana24@gmail.com>
2024-01-13 18:32:34 -0500

    Routes Setup

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = 3000;
+
+app.use(cors());
+
+const { readdirSync } = require("fs");
+readdirSync("./routes").map((file) =>
+  app.use("/", require("./routes/" + file))
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+require("dotenv").config();
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
